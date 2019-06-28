1ae307b3f5bea3ca44f60031f09e38df83e158bb
Karl <x>
2019-06-28 22:46:37 +0200

    added skeleton for recursive implementation

--- common.lisp
+++ common.lisp
@@ -3,9 +3,28 @@
 (in-package :pm)
 
 (defstruct coin
+  ;; Unique identifier for the coin, can be an index etc.
+  ;; Useful to return the solution set of coins.
+  (id 0)
   ;; Face value of the coin. Stored as 'i' for the value '2^i'.
   (face 0 :type fixnum)
   ;; Numismatic value, or 'weight' of coin. Real number in the most general
   ;; case.
-  (weigth 0))
+  (weight 0)
+  ;; Tail, to keep track of which coin is in which package.
+  (tail nil :type (or null coin)))
+
+(defun coin-order (coin1 coin2)
+  "Order relation to sort coins in non decreasing order of face first and then
+in non decreasing order of weight.
+TODO: use nif here."
+  (let ((f1 (coin-face coin1))
+	(f2 (coin-face coin2)))
+    (if (< f1 f2)
+	T
+	(if (= f1 f2)
+	    (if (<= (coin-weight coin1) (coin-weight coin2))
+		T
+		nil)
+	    nil))))
 
---
+++ recursive.lisp
@@ -0,0 +1,16 @@
+;;;; Recursive algorithm for package-merge.
+;;;; This is the general case of package-merge, not specialized for
+;;;; length-limited Huffman coding.
+
+(defun pm-rec (I X)
+  "I: I: Vector of coins, sorted by non decreasing face value. And inside the 
+         same values, by non decreasing order of weight.
+      X: Vector of i values in increasing order. The number X represents is a 
+         decomposition in powers of 2, being 2^i."
+
+  )
+
+(defun insert (coin I)
+  "Insert a new coin in I at the correct ordered position."
+  
+  )
