3f17656876249ef1ef8f9a6ca7924ce2b72032b9
Karl <x>
2019-07-15 18:41:36 +0200

    perform 10k cross-tests each time

--- cross-test.lisp
+++ cross-test.lisp
@@ -1,27 +1,30 @@
 ;;;; Cross-checking the two implementations. Uses random tests.
 
-(let* ((ncoins (1+ (random 20)))
-       (nX (1+ (random 10)))
-       (maxweight (1+ (random 40)))
-       (maxface (1+ (random 5))) ;+-
-       (faces (make-array ncoins :element-type 'fixnum))
-       (weights (make-array ncoins :element-type 'fixnum))
-       (X (make-array nX :element-type 'fixnum))
-       (Xval 0)
-       (recur)
-       (nonrecur))
-  ;; Filling all with random data.
-  (loop for i from 0 below ncoins do
-    (setf (aref weights i) (random maxweight))
-    (setf (aref faces i) (- (random (* 2 maxface)) maxface)))
-  (loop for i from 0 below nX do
-    (setf (aref X i) (- (random (* 2 maxface)) maxface)))
-  (setf X (delete-duplicates (sort X #'>)))
-  ;; Computing the face value of X.
-  (loop for f across X do
-	(incf Xval (expt 2 f)))
-  ;; Computing the two results.
-  (psetf recur (pm:pm-rec faces weights X)
-	 nonrecur (pm:pm-nonrec faces weights X))
-  (format t "~A~%~A~%~A~%~A~%~A" faces weights X recur nonrecur)
-  (check-inds recur nonrecur faces weights Xval))
+(dotimes (it 10000)
+  (let* ((ncoins (1+ (random 20)))
+	(nX (1+ (random 10)))
+	(maxweight (1+ (random 40)))
+	(maxface (1+ (random 5)))	;+-
+	(faces (make-array ncoins :element-type 'fixnum))
+	(weights (make-array ncoins :element-type 'fixnum))
+	(X (make-array nX :element-type 'fixnum))
+	(Xval 0)
+	(recur)
+	(nonrecur))
+   ;; Filling all with random data.
+   (loop for i from 0 below ncoins do
+     (setf (aref weights i) (random maxweight))
+     (setf (aref faces i) (- (random (* 2 maxface)) maxface)))
+   (loop for i from 0 below nX do
+     (setf (aref X i) (- (random (* 2 maxface)) maxface)))
+   (setf X (delete-duplicates (sort X #'>)))
+   ;; Computing the face value of X.
+   (loop for f across X do
+     (incf Xval (expt 2 f)))
+   ;; Computing the two results.
+   (psetf recur (pm:pm-rec faces weights X)
+	  nonrecur (pm:pm-nonrec faces weights X))
+   ;; (format t "~A~%~A~%~A~%~A~%~A" faces weights X recur nonrecur)
+   (check-inds recur nonrecur faces weights Xval)))
+
+(format t "Random cross-testing: PASS~%")
